514c7b54cea8d4eef2a3da80b65d467cebc29413
Mauricio Pinto <mauteixp@motorola.com>
2023-02-01 10:03:57 -0400

    feat: add validation to create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/CreateUser.dto';
 import { UserRepository } from './user.repository';
 
 @Controller('/users')
@@ -6,7 +7,7 @@ export class UserController {
   constructor(private userRepository: UserRepository) {}
 
   @Post()
-  async createUser(@Body() userData) {
+  async createUser(@Body() userData: CreateUserDTO) {
     this.userRepository.save(userData);
     return userData;
   }
