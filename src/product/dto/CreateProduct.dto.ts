d0681fb5aa2cf9c089e304b40afdaa6f53a3f260
Mauricio Pinto <mauteixp@motorola.com>
2023-02-01 10:44:08 -0400

    feat: add create product dto

---
+++ src/product/dto/CreateProduct.dto.ts
@@ -0,0 +1,66 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUrl,
+  MaxLength,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+  @Min(1)
+  value: number;
+
+  @IsNumber()
+  @Min(0)
+  amount: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(1000)
+  description: string;
+
+  @ValidateNested()
+  @IsArray()
+  @ArrayMinSize(3)
+  @Type(() => ProductCharacteristicDTO)
+  characteristics: ProductCharacteristicDTO[];
+
+  @ValidateNested()
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => ProductImageDTO)
+  images: ProductImageDTO[];
+
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+}
+
+export class ProductImageDTO {
+  @IsUrl()
+  url: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
+
+export class ProductCharacteristicDTO {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateProductDto } from './dto/CreateProduct.dto';
 import { ProductRepository } from './product.repository';
 
 @Controller('/products')
@@ -6,7 +7,7 @@ export class ProductController {
   constructor(private productRepository: ProductRepository) {}
 
   @Post()
-  async createProduct(@Body() productData) {
+  async createProduct(@Body() productData: CreateProductDto) {
     this.productRepository.save(productData);
     return productData;
   }
