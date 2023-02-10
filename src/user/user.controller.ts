9908c2741c6468844d8f0edfd3aef99b10a1f6a5
Mauricio Pinto <mauteixp@motorola.com>
2023-02-10 13:52:56 -0400

    feat: create list user dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { CreateUserDTO } from './dto/CreateUser.dto';
 import { UserEntity } from './user.entity';
 import { UserRepository } from './user.repository';
 import { v4 as uuid } from 'uuid';
+import { ListUserDto } from './dto/ListUser.dto';
 
 @Controller('/users')
 export class UserController {
@@ -17,11 +18,19 @@ export class UserController {
     userEntity.id = uuid();
 
     this.userRepository.save(userEntity);
-    return { id: userEntity.id, message: 'user created successfully' };
+    return {
+      user: new ListUserDto(userEntity.id, userEntity.name),
+      message: 'user created successfully',
+    };
   }
 
   @Get()
   async listUsers() {
-    return this.userRepository.list();
+    const savedUsers = await this.userRepository.list();
+    const usersList = savedUsers.map(
+      (user) => new ListUserDto(user.id, user.name),
+    );
+
+    return usersList;
   }
 }
