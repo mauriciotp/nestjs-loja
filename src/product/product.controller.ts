2729eec642fedf0b958fa65f2a5e20d775adc55d
Mauricio Pinto <mauteixp@motorola.com>
2023-02-16 12:50:41 -0400

    feat: create product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateProductDto } from './dto/CreateProduct.dto';
+import { ProductEntity } from './product.entity';
 import { ProductRepository } from './product.repository';
+import { v4 as uuid } from 'uuid';
+import { ListProductDTO } from './dto/ListProduct.dto';
 
 @Controller('/products')
 export class ProductController {
@@ -8,8 +11,19 @@ export class ProductController {
 
   @Post()
   async createProduct(@Body() productData: CreateProductDto) {
-    this.productRepository.save(productData);
-    return productData;
+    const productEntity = new ProductEntity();
+    productEntity.id = uuid();
+    productEntity.amount = productData.amount;
+    productEntity.category = productData.category;
+    productEntity.characteristics = productData.characteristics;
+    productEntity.name = productData.name;
+    productEntity.userId = productData.userId;
+    productData.value = productData.value;
+    productEntity.description = productData.description;
+
+    const savedProduct = this.productRepository.save(productEntity);
+
+    return savedProduct;
   }
 
   @Get()
---
+++ src/product/product.entity.ts
@@ -0,0 +1,21 @@
+export class ProductEntity {
+  id: string;
+  userId: string;
+  name: string;
+  value: string;
+  amount: number;
+  description: string;
+  characteristics: ProductCharacteristic[];
+  images: ProductImage[];
+  category: string;
+}
+
+class ProductImage {
+  url: string;
+  description: string;
+}
+
+class ProductCharacteristic {
+  name: string;
+  description: string;
+}
